af760f9a88deedeb5278f4d2dd95ca3922b8e322
KatoMono Enkeli <k4t0mono@gmail.com>
2019-05-25 20:40:52 -0300

    fixing tests

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-# from routes import app_yiff, app_main
 
 
 # Init the app
 app = Flask(__name__)
-# app.register_blueprint(app_yiff)
-# app.register_blueprint(app_main)
 
 # Config the data base
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI', 'sqlite:///yiff.sqlite')
@@ -17,10 +14,14 @@ app.config['JSON_SORT_KEYS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Registering the routes
+from routes import *
+app.register_blueprint(app_person)
+app.register_blueprint(app_yiff)
+app.register_blueprint(app_main)
+
+
 if __name__ == '__main__':
     port = os.getenv('PORT', 58913)
-
-    from routes import app_person
-    app.register_blueprint(app_person)
 
     app.run(host='0.0.0.0', port=port, debug=True)
